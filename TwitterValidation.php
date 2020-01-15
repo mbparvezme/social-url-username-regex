00cd7d42011bc8d7b18ec9cf3fba9a1f26ece615
M. B. Parvez Ron <mbpron@gmail.com>
2020-01-15 18:28:14 +0600

    Update TwitterValidation.php

--- TwitterValidation.php
+++ TwitterValidation.php
@@ -21,49 +21,49 @@ class TwitterValidation
 	 * Variable that contains the RegEx
 	 * to validate an URl
 	 */
-  protected $urlRegEx = '/(http(s)?:\/\/)?(www\.)?twitter\.com\/(#!\/)?@?([a-zA-Z0-9_]{1,15})/i';
+	protected $urlRegEx = '/(http(s)?:\/\/)?(www\.)?twitter\.com\/(#!\/)?@?([a-zA-Z0-9_]{1,15})/i';
 
 
 	/**
 	 * Variable that contains the RegEx
 	 * to validate an username
 	 */
-  protected $unameRegEx = '/^@?([a-z0-9_]{1,15})$/i';
+	protected $unameRegEx = '/^@?([a-z0-9_]{1,15})$/i';
 
-  public $excludeAt = FALSE;
+	public $excludeAt = FALSE;
 
-  public $input;
-  
-  public $url;
-  
-  public $username;
+	public $input;
 
-  public verify(){
-    // Verify URL
-    if( $this->verifyUrl() )
-      return $this->url;
-    // Verify Username
-    if( $this->verifyUsername() )
-      return $this->username;
-    
-    // If both mismatch
-    return FALSE;
-  }
+	public $url;
 
-  protected verifyUrl(){
-    if(preg_match($this->urlRegEx, $this->input, $matches)){
-      $this->url = $matches[5];
-      return TRUE;
-    }
-    return FALSE;
-  }
+	public $username;
 
-  protected verifyUsername(){
-    if(preg_match($this->unameRegEx, $this->input)){
-      $this->username = (!$this->excludeAt ? $this->input : str_replace("@", "", $this->input));
-      return TRUE;
-    }
-    return FALSE;
-  }
+	public verify(){
+		// Verify URL
+		if( $this->verifyUrl() )
+			return $this->url;
+		// Verify Username
+		if( $this->verifyUsername() )
+			return $this->username;
+
+		// If both mismatch
+		return FALSE;
+	}
+
+	protected verifyUrl(){
+		if(preg_match($this->urlRegEx, $this->input, $matches)){
+			$this->url = $matches[5];
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	protected verifyUsername(){
+		if(preg_match($this->unameRegEx, $this->input)){
+			$this->username = (!$this->excludeAt ? $this->input : str_replace("@", "", $this->input));
+			return TRUE;
+		}
+		return FALSE;
+	}
 
 }
